2c41d2a4ea8761442da6910e36e13cfdcb61d005
miil1215 <mikhail.ilin@netcracker.com>
2017-01-25 17:08:03 +0300

    hooks

---
+++ init-git-flow.cmd
@@ -0,0 +1,36 @@
+@echo off
+
+rem Use this script for configure git flow plugin for this project.
+
+rem Check gitflow is installed
+echo Check gitflow version
+git flow version
+if errorLevel 1 (
+	echo GitFlow extensions is not installed. Break process.
+	exit 1
+)
+
+echo Configure gitflow paramters
+rem Set main branch names:
+git config gitflow.branch.master master
+git config gitflow.branch.develop develop
+
+rem Set branch prefixes:
+git config gitflow.prefix.feature feature/
+git config gitflow.prefix.bugfix bugfix/
+git config gitflow.prefix.release release/
+git config gitflow.prefix.hotfix hotfix/
+git config gitflow.prefix.support support/
+git config gitflow.prefix.versiontag v
+
+rem Set hooks path for project:
+set CURRENT_DIR=%cd%
+set HOOKS_TARGET_DIR=%CURRENT_DIR%\.git\hooks
+set HOOKS_SOURCE_DIR=%CURRENT_DIR%\hooks
+git config gitflow.path.hooks %HOOKS_TARGET_DIR% 
+
+echo Copy hooks
+rem Copy hooks
+xcopy /S /Y /Q %HOOKS_SOURCE_DIR% %HOOKS_TARGET_DIR%
+
+echo Configured
