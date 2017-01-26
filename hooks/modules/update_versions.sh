73713846e84be0ba26f731cf2d1b4a16d7a1120a
miil1215 <mikhail.ilin@netcracker.com>
2017-01-26 13:27:57 +0300

    changes

--- hooks/modules/update_versions.sh
+++ hooks/modules/update_versions.sh
@@ -16,6 +16,14 @@ if [ $? -ne 0 ]; then
 fi
 mvn versions:commit
 
+VERSION_REGEX=[0-9]+\.[0-9]+\.[0-9]+
+
+LEN=$(expr match $VERSION_FOR_UPDATE $VERSION_REGEX)
+if [ $LEN -gt 0 ]; then
+	VERSIONS_FOR_NPM=$(expr substr $VERSION_FOR_UPDATE, 1, $LEN)	
+	sed -i -- "s/\"version\":\s*\".*\"/\"version\":\"$VERSION_FOR_UPDATE\"/g" client/package.json
+fi
+
 sed -i -- "s/\"tag\":\s*\".*\"/\"tag\":\"$VERSION_FOR_UPDATE\"/g" client/package.json
 
 git add */pom.xml
@@ -23,5 +31,5 @@ git add pom.xml
 git add client/package.json
 git commit -m "Update project version to $VERSION_FOR_UPDATE"
 
-#return zero besides if we continue release there is nothing for commit will be and git commit returns 1
+#return zero because if we continue release there is nothing for commit will be and git commit returns 1
 exit 0
